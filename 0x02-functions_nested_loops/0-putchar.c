92d1e4dddcd12adbdddde39860e7c0ed94b7fa71
damaris-lab <damarisnyawiragacheru@gmail.com>
2023-12-20 09:07:54 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *_putchar - prints putchar
+ *followed by a new line
+ *
+ * Return: return 0 on success
+ */
+int _putchar(void)
+{
+	return (write(1, &c, 1));
+}
