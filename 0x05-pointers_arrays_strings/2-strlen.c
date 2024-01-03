c0ff6fd6a7d011e93e428905c83430e85c900f42
damaris-lab <damarisnyawiragacheru@gmail.com>
2024-01-03 13:20:31 -0800

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int i, len;
+
+	for (i = 0; s[i] != '\0'; i++)
+
+	return (len);
+}
