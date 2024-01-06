695001d9c9246b495bc89173983f64c3db161ccd
damaris-lab <damarisnyawiragacheru@gmail.com>
2024-01-05 16:47:38 -0800

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *
+ *
+ */
+char *_strcat(char *dest, char *src);
+int = 0
+{
+	i=o
+	while (dest[1]! = '/0')
+	{
+	i++
+	}
+	j=o
+	while (src[j]! = '/0'_)
+	{
+		dest[i] = src[i]
+                j++;
+                i++;
+	}
+	
+	return dest;
+}	
+
