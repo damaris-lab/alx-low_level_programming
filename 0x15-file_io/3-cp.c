1fde6b15386e04d0b19fb4480f2c74c2a3575577
damaris-lab <damarisnyawiragacheru@gmail.com>
2024-03-02 16:32:12 -0800

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,67 +1,47 @@
 #include "main.h"
-
 /**
- * main - copies the contents of one file to another file
- * @argc: The argument count to main
- * @argv: An array of strings of arguments to main
+ * main - copies content of a file to another file
+ * @argc: argument count
+ * @argv: pointer to argument list
  *
- * Return: Always 0 (Success)
+ * Return: 0 for success
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	int fd_from, fd_to;
-	ssize_t ret_read, ret_write;
-	char buffer[BUFSIZE];
+	int fd;
+	int fd2;
+	int nb_read;
+	char buf[1024];
 
-	if (argc < 3)
+	if (argc != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from < 0)
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: ");
+		dprintf(STDERR_FILENO, "Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_to < 0)
+	nb_read = read(fd, buf, 1024);
+	fd2 = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
+	if (fd2 == -1)
 	{
-		dprintf(2, "Error: Cant write to %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-	while ((ret_read = read(fd_from, buffer, BUFSIZE)) > 0)
+	write(fd2, buf, nb_read);
+	if (close(fd) == -1)
 	{
-		ret_write = write(fd_to, buffer, ret_read);
-		if (ret_write < 0)
-		{
-			dprintf(2, "Error: Cant write to %s\n", argv[2]);
-			exit(99);
-		}
-	}
-	if (ret_read < 0)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	close_descriptor(fd_from);
-	close_descriptor(fd_to);
-	return (0);
-}
-/**
- * close_descriptor - closes a file descriptor
- * @fd: A file descriptor
- *
- * Return: Nothing.
- */
-void close_descriptor(int fd)
-{
-	int close_ret;
-
-	close_ret = close(fd);
-	if (close_ret < 0)
-	{
-		dprintf(2, "Error: Cant close fd %i\n", fd);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
+	if (close(fd2) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
+		exit(100);
+	}
+	return (0);
 }
