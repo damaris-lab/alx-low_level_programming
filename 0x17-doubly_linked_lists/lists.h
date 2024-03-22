c18ed77e1e47778334b2a325d77a56f4c98ba2c2
damaris-lab <damarisnyawiragacheru@gmail.com>
2024-03-22 16:43:34 -0700

    0-print_dlistint.c

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,25 +1,31 @@
 #ifndef LISTS_H
 #define LISTS_H
-
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
- * struct listint_s - singly linked list
+ * struct dlistint_s - doubly linked list
  * @n: integer
+ * @prev: points to the previous node
  * @next: points to the next node
  *
- * Description: singly linked list node structure
+ * Description: doubly linked list node structure
+ *
  */
-typedef struct listint_s
+typedef struct dlistint_s
 {
 	int n;
-	struct listint_s *next;
-} listint_t;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
+} dlistint_t;
 
-size_t print_listint(const listint_t *h);
-listint_t *add_nodeint_end(listint_t **head, const int n);
-void free_listint(listint_t *head);
-int is_palindrome(listint_t **head);
+size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+void free_dlistint(dlistint_t *head);
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
+int sum_dlistint(dlistint_t *head);
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 
-#endif /* LISTS_H */
+#endif
