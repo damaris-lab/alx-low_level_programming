6cf508a732bce84d8ba3476a49b756740b354460
damaris-lab <damarisnyawiragacheru@gmail.com>
2024-04-18 18:49:39 -0700

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,19 +9,20 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table = malloc(sizeof(hash_table_t));
-	unsigned long int i;
+	hash_table_t *hash_table;
+	unsigned int i;
 
-	if (table == NULL)
+	hash_table = (hash_table_t *) malloc(sizeof(hash_table_t));
+	if (hash_table == NULL)
 		return (NULL);
-	table->size = size;
-	table->array = malloc(sizeof(hash_node_t *) * size);
-	if (table->array == NULL)
+	hash_table->size = size;
+	hash_table->array = (hash_node_t **) calloc(size, sizeof(hash_node_t *));
+	if (hash_table->array == NULL)
 	{
-		free(table);
+		free(hash_table);
 		return (NULL);
 	}
-	for (i = 0; i < size; i++)
-		table->array[i] = NULL;
-	return (table);
+	for (i = 0; i < hash_table->size; i++)
+		hash_table->array[i] = NULL;
+	return (hash_table);
 }
